9f1492265203634428c8abf5eb08de3768ed36ed
yvan vander sanden <yvan@attr-x.net>
2026-04-24 00:00:56 +0200

    feat: update GitHub Actions workflow for deployment and configure Astro site settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://yvanvds.github.io',
+  base: '/cursus-esthetica',
   integrations: [mdx()],
 });
